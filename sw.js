1f5d3c7f0e42dd5ecbff32d716f197f4e5f4627a
Dyuzz Yu <13070895895@163.com>
2023-09-11 16:00:36 +0800

    google verifaction

--- sw.js
+++ sw.js
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-8de36d3fd07627b19105.js"
   },
   {
-    "url": "app-1e4fc83cd90ff2328638.js"
+    "url": "app-1d5dcfa6da0b9b786f4a.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-7ee59ccf20e51ee39e83.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0d959bf69e3100058df32082713fb3cc"
+    "revision": "79437f03d38a9b388f64fc45f01915f2"
   },
   {
     "url": "polyfill-2259bef4142eb33966c2.js"
@@ -76,7 +76,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1e4fc83cd90ff2328638.js`))) {
+  if (!resources || !(await caches.match(`/app-1d5dcfa6da0b9b786f4a.js`))) {
     return await fetch(event.request)
   }
 
